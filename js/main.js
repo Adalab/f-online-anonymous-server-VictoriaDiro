d2a1ad94d5d94abce7a1c8e7f4b28e1d3c2a4124
joseramon.afonso <joseramon.afonso@devo.com>
2019-07-09 16:58:36 +0200

    improve styles

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@
 window.onload = init;
 
 function init() {
+  document.querySelector('.nav-close').style.display = 'none';
   document.querySelector('.nav-trigger').addEventListener('click', openMenu);
   document.querySelector('.nav-close').addEventListener('click', closeMenu);
   document.querySelector('.nav > ul').style.display = 'none';
@@ -13,6 +14,10 @@ function openMenu() {
 
   if(state == 'none') {
     document.querySelector('.nav > ul').style.display = 'block';
+    document.querySelector('.nav-trigger').style.display = 'none';
+    document.querySelector('.nav-close').style.display = 'block';
+    document.querySelector('.page__main-title').style.display = 'none';
+    document.querySelector('.page__footer').style.display = 'none';
   } 
 }
 
@@ -21,5 +26,9 @@ function closeMenu() {
 
   if(state == 'block') {
     document.querySelector('.nav > ul').style.display = 'none';
+    document.querySelector('.nav-close').style.display = 'none';
+    document.querySelector('.nav-trigger').style.display = 'block';
+    document.querySelector('.page__main-title').style.display = 'block';
+    document.querySelector('.page__footer').style.display = 'block';
   };
 }
